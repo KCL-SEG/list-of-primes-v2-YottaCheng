e832a590ef7a919d9f6fbfb25407bd705b43ae31
Ziyao Cheng <yottacheng3@gmail.com>
2023-12-07 23:53:21 +0000

    new

--- primes.py
+++ primes.py
@@ -2,5 +2,14 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer")
+
+    list_of_primes = []
+    num = 2
+    while len(list_of_primes) < number_of_primes:
+        if all(num % prime != 0 for prime in list_of_primes):
+            list_of_primes.append(num)
+        num += 1
+
+    return list_of_primes
